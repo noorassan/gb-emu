e4fddc687089a98ffcb1965e111924bd4d6c4148
Naisan Noorassa <naisan99@gmail.com>
2021-09-13 15:53:59 -0700

    Refactor high ram memory access & remove redundant memory.

--- include/bus.h
+++ include/bus.h
@@ -16,39 +16,52 @@
 
 #define POLL_INTERVAL 210672
 
+#define DMA 0xFF46
+#define SB 0xFF01 // serial bus
+#define SC 0xFF02 // serial control
+#define IF 0xFF0F // interrupt flag
+
 
 class Bus {
-public:
-    Bus(GameboyDriver *driver);
-    ~Bus() = default;
+    public:
+        Bus(GameboyDriver *driver);
+        ~Bus() = default;
 
-private:
-    CPU cpu;
-    PPU ppu;
-    APU apu;
-    Timer timer;
-    Controls controls;
-    std::shared_ptr<Cartridge> cart;
-    std::array<uint8_t, 8 * KB> ram;
-    std::array<uint8_t, 0xFF> zero_page_ram;
+    private:
+        CPU cpu;
+        PPU ppu;
+        APU apu;
+        Timer timer;
+        Controls controls;
+        std::shared_ptr<Cartridge> cart;
+        std::array<uint8_t, 8 * KB> ram;
+        std::array<uint8_t, 0x7F> high_ram;
 
-    GameboyDriver *driver;
+        GameboyDriver *driver;
 
-public:
-    uint8_t cpuRead(uint16_t addr);
-    void cpuWrite(uint16_t addr, uint8_t data);
+    public:
+        uint8_t cpuRead(uint16_t addr);
+        void cpuWrite(uint16_t addr, uint8_t data);
 
-    void requestInterrupt(INTERRUPT intr);
+        void requestInterrupt(INTERRUPT intr);
 
-    void reset();
+        void reset();
 
-    void run();
+        void run();
 
-    void insertCartridge(const std::shared_ptr<Cartridge> cart);
+        void insertCartridge(const std::shared_ptr<Cartridge> cart);
 
-    void saveState(const std::string &filename);
-    void loadState(const std::string &filename);
+        void saveState(const std::string &filename);
+        void loadState(const std::string &filename);
 
-private:
-    bool handleDMA(uint16_t addr, uint8_t data);
+    private:
+        void handleDMA(uint8_t data);
+
+        void handleIOWrite(uint16_t addr, uint8_t data);
+        uint8_t handleIORead(uint16_t addr);
+
+    private:
+        uint8_t sb;
+        uint8_t sc;
+        uint8_t intr_flag;
 };
--- src/bus.cc
+++ src/bus.cc
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "bus.h"
 
 Bus::Bus(GameboyDriver *driver) : ppu(driver), apu(driver), controls(driver) {
@@ -24,27 +22,11 @@ void Bus::cpuWrite(uint16_t addr, uint8_t data) {
         ram[addr & 0x1FFF] = data;
     } else if (addr >= 0xFE00 && addr < 0xFEA0) {
         ppu.cpuWrite(addr, data);
-    } else if (addr >= 0xFF00 && addr <= 0xFFFF) {
-        // Writes in this address range are delegated to devices
-
-        if (timer.regWrite(addr, data)) {
-            return;
-        }
-        if(ppu.regWrite(addr, data)) {
-            return;
-        }
-        if (controls.regWrite(addr, data)) {
-            return;
-        }
-        if (apu.regWrite(addr, data)) {
-            return;
-        }
-        if (handleDMA(addr, data)) {
-            return;
-        }
-
-        zero_page_ram[addr & 0x00FF] = data;
-    } 
+    } else if (addr >= 0xFF00 && addr < 0xFF4C) {
+        handleIOWrite(addr, data);
+    } else if (addr >= 0xFF80) {
+        high_ram[addr & 0x7F] = data;
+    }
 }
 
 uint8_t Bus::cpuRead(uint16_t addr) {
@@ -58,27 +40,13 @@ uint8_t Bus::cpuRead(uint16_t addr) {
         return ram[addr & 0x1FFF];
     } else if (addr >= 0xFE00 && addr < 0xFEA0) {
         return ppu.cpuRead(addr);
-    } else if (addr >= 0xFF00 && addr <= 0xFFFF) {
-        // Reads in this address range are delegated to devices
-
-        uint8_t val;
-        if (timer.regRead(addr, val)) {
-            return val;
-        }
-        if (ppu.regRead(addr, val)) {
-            return val;
-        }
-        if (apu.regRead(addr, val)) {
-            return val;
-        }
-        if (controls.regRead(addr, val)) {
-            return val;
-        }
-
-        return zero_page_ram[addr & 0x00FF];
+    } else if (addr >= 0xFF00 && addr < 0xFF4C) {
+        return handleIORead(addr);
+    } else if (addr >= 0xFF80) {
+        return high_ram[addr & 0x7F];
     }
 
-    return 0;
+    return 0x00;
 }
 
 void Bus::requestInterrupt(INTERRUPT intr) {
@@ -87,17 +55,7 @@ void Bus::requestInterrupt(INTERRUPT intr) {
 
 void Bus::reset() {
     for (auto &i : ram) i = 0x00;
-    for (auto &i : zero_page_ram) i = 0x00;
-
-    // reset memory locations to default values
-    zero_page_ram[0x05] = 0x00; zero_page_ram[0x06] = 0x00; zero_page_ram[0x07] = 0x00; zero_page_ram[0x10] = 0x80;
-    zero_page_ram[0x11] = 0xBF; zero_page_ram[0x12] = 0xF3; zero_page_ram[0x14] = 0xBF; zero_page_ram[0x16] = 0x3F;
-    zero_page_ram[0x17] = 0x00; zero_page_ram[0x19] = 0xBF; zero_page_ram[0x1A] = 0x7F; zero_page_ram[0x1B] = 0xFF;
-    zero_page_ram[0x1C] = 0x9F; zero_page_ram[0x1E] = 0xBF; zero_page_ram[0x20] = 0xFF; zero_page_ram[0x21] = 0x00;
-    zero_page_ram[0x22] = 0x00; zero_page_ram[0x23] = 0xBF; zero_page_ram[0x24] = 0x77; zero_page_ram[0x25] = 0xF3;
-    zero_page_ram[0x26] = 0xF1; zero_page_ram[0x40] = 0x91; zero_page_ram[0x42] = 0x00; zero_page_ram[0x43] = 0x00;
-    zero_page_ram[0x45] = 0x00; zero_page_ram[0x47] = 0xFC; zero_page_ram[0x48] = 0xFF; zero_page_ram[0x49] = 0xFF;
-    zero_page_ram[0x4A] = 0x00; zero_page_ram[0x4B] = 0x00; zero_page_ram[0xFF] = 0x00;
+    for (auto &i : high_ram) i = 0x00;
 
     cpu.reset();
     ppu.reset();
@@ -139,15 +97,55 @@ void Bus::loadState(const std::string &filename) {
     ifs.close();
 }
 
-bool Bus::handleDMA(uint16_t addr, uint8_t data) {
-    if (addr != 0xFF46) {
-        return false;
-    }
-
+void Bus::handleDMA(uint8_t data) {
     uint16_t dma_addr = data << 8;
     for (uint8_t i = 0; i < OAM_SIZE; i++) {
         cpuWrite(OAM_START + i, cpuRead(dma_addr + i));
     }
+}
 
-    return true;
+void Bus::handleIOWrite(uint16_t addr, uint8_t data) {
+    if (timer.regWrite(addr, data)) {
+        return;
+    }
+    if(ppu.regWrite(addr, data)) {
+        return;
+    }
+    if (controls.regWrite(addr, data)) {
+        return;
+    }
+    if (apu.regWrite(addr, data)) {
+        return;
+    }
+
+    switch(addr) {
+        case DMA: handleDMA(data); break;
+        case SB: sb = data; break;
+        case SC: sc = data; break;
+        case IF: intr_flag = data; break;
+    }
+}
+
+uint8_t Bus::handleIORead(uint16_t addr) {
+    uint8_t val;
+    if (timer.regRead(addr, val)) {
+        return val;
+    }
+    if (ppu.regRead(addr, val)) {
+        return val;
+    }
+    if (apu.regRead(addr, val)) {
+        return val;
+    }
+    if (controls.regRead(addr, val)) {
+        return val;
+    }
+
+    switch(addr) {
+        case SB: return sb; break;
+        case SC: return sc; break;
+        case IF: return intr_flag; break;
+    }
+
+    return 0x00;
 }
